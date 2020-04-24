c1b3ef3d3969f6abf828725a8ab9add3e2b0412b
tjmam <jack.murray.morgan@gmail.com>
2020-04-24 22:10:39 +0000

    Updated Wordpress v5.4 Template

--- wp-config.php
+++ wp-config.php
@@ -28,7 +28,7 @@ define( 'DB_NAME', 'wordpress' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'v8W1dshp24MQ' );
+define( 'DB_PASSWORD', 'KAJDQBmVX1Zi' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
